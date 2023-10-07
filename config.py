2e8f66d43b956d5503898143b311ee4ed56a9238
shadowXalone <146691704+shadowXalone@users.noreply.github.com>
2023-10-08 00:15:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 import logging
 class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    Channel_id = os.environ.get("Channel_id", "")
+    API_ID = int(os.environ.get("API_ID", "28839759"))
+    API_HASH = os.environ.get("API_HASH", "c5ec8c471ec25acc97db0f5089c7a63a")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6395985829:AAFrzpVRCG8pkn3yJcA1QO0iaJeVuqdrhf4")
+    Channel_id = os.environ.get("Channel_id", "-1001597165437")
     Drivebuzz_crypt = os.environ.get("Drivebuzz_crypt", "")
     Drivefire_crypt = os.environ.get("Drivefire_crypt", "")
     Jiodrive_crypt = os.environ.get("Jiodrive_crypt", "")
